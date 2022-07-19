b7539681dece464d20dfa2e5b59072b8d4330f9e
Mohammed El-kabli <melkabli00@gmail.com>
2022-07-19 21:06:44 +0100

    project initialization

--- src/main/java/com/example/ecom/EcomApplication.java
+++ src/main/java/com/example/ecom/EcomApplication.java
@@ -1,4 +1,4 @@
-package com.app.ecom;
+package com.example.ecom;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EcomApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(EcomApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(EcomApplication.class, args);
+	}
 
 }
--- src/test/java/com/example/ecom/EcomApplicationTests.java
+++ src/test/java/com/example/ecom/EcomApplicationTests.java
@@ -1,4 +1,4 @@
-package com.app.ecom;
+package com.example.ecom;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class EcomApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
